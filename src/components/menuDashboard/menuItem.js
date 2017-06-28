8571a0c9d57c9689b4d448b1bb8e0603a60b97a8
BriOD <briodonoghue@gmail.com>
2017-06-28 19:21:16 -0400

    stubed out MenuItem

--- src/components/menuDashboard/menuItem.js
+++ src/components/menuDashboard/menuItem.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
 
 class MenuItem extends React.Component {
+  render() {
+    return (
+      <div className='ui centered card'>
+        <div className='content'>
+          <div className='header'>
+            {this.props.title}
+          </div>
+          <div className='meta'>
+            {this.props.description}
+          </div>
+          <div className='meta'>
+            {this.props.price}
+          </div>
+          <div className='extra content'>
+            <span className='right floated edit icon'>
+              <i className='edit icon' />
+            </span>
+            <span className='right floated trash icon'>
+              <i className='trash icon' />
+            </span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
 }
 
