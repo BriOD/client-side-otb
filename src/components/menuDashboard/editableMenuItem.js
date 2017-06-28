2a1de663c2df5b4a419bb33629542d05349d5d04
BriOD <briodonoghue@gmail.com>
2017-06-28 19:06:56 -0400

    stubbed out EditableMenuItem

--- src/components/menuDashboard/editableMenuItem.js
+++ src/components/menuDashboard/editableMenuItem.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
+import MenuItemForm from './menuItemForm';
+import MenuItem from './menuItem';
 
 // returns either a MenuItemFrom or a MenuItem based on the prop editFromOpen
 
 class EditableMenuItem extends React.Component {
+  render() {
+    if (this.props.editFormOpen) {
+      return (
+        <MenuItemForm
+          title={this.props.title}
+          description={this.props.description}
+          price={this.props.price}
+        />
+      );
+    } else {
+      return (
+        <MenuItem
+        title={this.props.title}
+        description={this.props.description}
+        price={this.props.price}
+        />
+      );
+    }
+  }
 
 }
 
---
+++ src/components/menuDashboard/menuItemForm.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+class MenuItemForm extends React.Component {
+
+}
+
+export default MenuItemFrom;
