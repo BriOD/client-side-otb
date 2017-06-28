29c87462e98efc6095b95dc11fc62f58386e4b5a
BriOD <briodonoghue@gmail.com>
2017-06-28 19:09:55 -0400

    stubbed out MenuItemForm

--- src/components/menuDashboard/menuItemForm.js
+++ src/components/menuDashboard/menuItemForm.js
@@ -1,6 +1,37 @@
 import React, { Component } from 'react';
 
 class MenuItemForm extends React.Component {
+  render() {
+    const submitText = this.props.title ? 'Update' : 'Create';
+    return (
+      <div className='ui centered card'>
+        <div className='content'>
+          <div className='ui form'>
+            <div className='field'>
+              <label>Title</label>
+              <input type='text' defaultValue={this.props.title} />
+            </div>
+            <div className='field'>
+              <label>Description</label>
+              <input type='text' defaultValue={this.props.description} />
+            </div>
+            <div className='field'>
+              <label>Price</label>
+              <input type='text' defaultValue={this.props.price} />
+            </div>
+            <div className='ui two bottom attached buttons'>
+              <button className='ui basic blue button'>
+                {submitText}
+              </button>
+              <button className='ui basic red button'>
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
 }
 
