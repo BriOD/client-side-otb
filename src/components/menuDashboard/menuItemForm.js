58e48de9e86fc9bec4af2b12ee8da3fe4287eb0f
BriOD <briodonoghue@gmail.com>
2017-06-29 11:28:28 -0400

    added state to MenuItemForm, and added interactivity to the input fields. typing in the input field is linked to updating state.

--- src/components/menuDashboard/menuItemForm.js
+++ src/components/menuDashboard/menuItemForm.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 
 class MenuItemForm extends Component {
+  state = {
+    title: this.props.title || '',
+    description: this.props.description || '',
+    price: this.props.price || ''
+  };
+  // if the MenuItemForm is creating a new menu item opposed to editing an existing one
+  // those props would be undefined. In that case, we initialize to a blank string
+
+  handleTitleChange = (e) => {
+    this.setState({ title: e.target.vale });
+  };
+
+  handleDescriptionChange = (e) => {
+    this.setState({ description: e.target.vale });
+  };
+
+  handlePriceChange = (e) => {
+    this.setState({ price: e.target.vale });
+  };
+
   render() {
     const submitText = this.props.title ? 'Update' : 'Create';
     return (
@@ -9,15 +29,27 @@ class MenuItemForm extends Component {
           <div className='ui form'>
             <div className='field'>
               <label>Title</label>
-              <input type='text' defaultValue={this.props.title} />
+              <input
+                type='text'
+                value={this.state.title}
+                onChange={this.handleTitleChange}
+              />
             </div>
             <div className='field'>
               <label>Description</label>
-              <input type='text' defaultValue={this.props.description} />
+              <input
+                type='text'
+                value={this.state.description}
+                onChange={this.handleDescriptionChange}
+              />
             </div>
             <div className='field'>
               <label>Price</label>
-              <input type='text' defaultValue={this.props.price} />
+              <input
+                type='text'
+                value={this.state.price}
+                onChange={this.handlePriceChange}
+              />
             </div>
             <div className='ui two bottom attached buttons'>
               <button className='ui basic blue button'>
