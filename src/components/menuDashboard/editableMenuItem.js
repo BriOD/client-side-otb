3efbc1988475966b569e401364c97eaab614b525
BriOD <briodonoghue@gmail.com>
2017-06-29 10:46:56 -0400

    added state to EditableMenuItem

--- src/components/menuDashboard/editableMenuItem.js
+++ src/components/menuDashboard/editableMenuItem.js
@@ -5,10 +5,15 @@ import MenuItem from './menuItem';
 // returns either a MenuItemFrom or a MenuItem based on the prop editFromOpen
 
 class EditableMenuItem extends Component {
+  state = {
+    editFromOpen: false,
+  };
+
   render() {
     if (this.props.editFormOpen) {
       return (
         <MenuItemForm
+        id={this.props.id}
           title={this.props.title}
           description={this.props.description}
           price={this.props.price}
@@ -17,6 +22,7 @@ class EditableMenuItem extends Component {
     } else {
       return (
         <MenuItem
+        id={this.props.id}
         title={this.props.title}
         description={this.props.description}
         price={this.props.price}
