0aa507291672d83f6dcc5ac0153f7440fd149f26
BriOD <briodonoghue@gmail.com>
2017-07-05 20:40:17 -0400

    created menuItemsReducer and stubbed out fake state data. also createx index.js in reducers folder, and set up boilerplate combineReducers function

---
+++ src/reducers/menuItemsreducer.js
@@ -0,0 +1,34 @@
+export default function() {
+  return [
+    {
+      title: 'Creamy Winter Vegetable Soup',
+      description: "lots of winter vegetables cooked in chicken stock, then blended. The soup's special finishing touch comes from cream and a spoonful of mustard",
+      price: '12.35',
+      id: cuid()
+    },
+    {
+      title: 'Gravlax',
+      description: "Gravlax is a Nordic speciality and consists of boned salmon dry-cured overnight in salt and sugar. The cured salmon can be eaten like smoked salmon, just with a trickle of lemon juice, or with a light sauce of cream cheese and herbs.",
+      price: '22.85',
+      id: cuid()
+    },
+    {
+      title: '6 OZ. CENTER-CUT SIRLOIN',
+      description: "Center-cut for tenderness. Lean, hearty and full of flavor",
+      price: '17.68',
+      id: cuid()
+    },
+    {
+      title: 'GRILLED CHICKEN ON THE BARBIE',
+      description: "Seasoned and wood-fire grilled chicken breast.",
+      price: '14.35',
+      id: cuid()
+    },
+    {
+      title: 'BONE-IN NATURAL CUT RIBEYE',
+      description: "Bone-in and extra marbled for maximum tenderness. Seasoned and wood-fire grilled over oak.",
+      price: '22.35',
+      id: cuid()
+    }
+  ]
+}
