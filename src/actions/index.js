08abe1fa3ab948044e6420482bbb4c47c891d9c0
BriOD <briodonoghue@gmail.com>
2017-07-06 16:36:41 -0400

    wired up action to fetch menuItems from API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,12 @@
+import axios from 'axios';
+
 export const FETCCH_MENU_ITEMS = 'fetch_menu_items';
 
 export function fetchMenuItems() {
+  const request = axios.get(`http://localhost:3001/api/menu_items`)
+
   return {
-    type:  FETCCH_MENU_ITEMS
+    type:  FETCCH_MENU_ITEMS,
+    payload: request
   };
 }
