4ac47b26568864c00c9380f8b01d8f7dc9f1b629
BriOD <briodonoghue@gmail.com>
2017-08-03 13:38:51 -0400

    added redux-form to project. and added it to reducers index file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import MenuItemsReducer from './menuItemsReducer'
 
 const rootReducer = combineReducers({
-  menuItems: MenuItemsReducer
+  menuItems: MenuItemsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
