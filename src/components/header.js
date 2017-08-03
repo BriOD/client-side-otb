de9500c57e36d7c1862df45bcaeab038a5547ba2
BriOD <briodonoghue@gmail.com>
2017-08-03 12:28:26 -0400

    added link components for the header navigation link

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render(){
@@ -7,8 +8,13 @@ class Header extends Component {
           <h1>On The Blvd.</h1>
           <img src="http://i.imgur.com/stGZhYjm.png" alt=""/>
           <div className="Button-div">
-            <button className="ui green basic button">Home</button>
-            <button className="ui green basic button">Lunch</button>
+            <Link className="ui green basic button" to="/">
+              Home
+            </Link>
+
+            <Link className="ui green basic button" to="/menu_items/new">
+              New Menu Item
+            </Link>
             <button className="ui green basic button">Dinner</button>
             <button className="ui green basic button">Todays Specials</button>
           </div>
