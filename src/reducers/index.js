e6f4685d63e4ac2357bd9fb0898d9b234f9ac54b
BriOD <briodonoghue@gmail.com>
2017-08-05 11:57:59 -0400

    fixed small bug in file name that occred when merging branches

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
-import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
-import MenuItemsReducer from './menuItemsReducer'
+import MenuItemsReducer from './menuItemsReducer';
+import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
   menuItems: MenuItemsReducer,
