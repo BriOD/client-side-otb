fe81df846ccb0cc8334e95bdff75e3960e7bff14
BriOD <briodonoghue@gmail.com>
2017-08-10 17:37:00 -0400

    added onClick event to sing in bttn. it now changes the state to either true or false based on the value of authenticate

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Header extends Component {
   authButton() {
-    return <button className="ui green basic button">Sign In</button>
+    if (this.props.authenticated) {
+      return <button onClick={() => this.props.authenticate(false)} className="ui green basic button">Sign Out</button>;
+    }
+    return <button onClick={() => this.props.authenticate(true)} className="ui green basic button">Sign In</button>;
   }
   render(){
     return(
@@ -23,4 +28,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return {authenticated: state.authenticated };
+}
+
+export default connect(mapStateToProps, actions)(Header);
