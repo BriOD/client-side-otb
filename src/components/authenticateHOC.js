498a0d2903b009981323bd9200952830a87f882e
BriOD <briodonoghue@gmail.com>
2017-08-12 13:28:54 -0400

    set up context for router in authenticateHOC

--- src/components/authenticateHOC.js
+++ src/components/authenticateHOC.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.history.push('/');
+      }
+    }
+
     render() {
-      console.log(this.props.authenticated);
+      console.log(this.context.router);
       return <ComposedComponent {...this.props} />
     }
   }
